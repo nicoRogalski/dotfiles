c612ce298f6e018f15d2911e271283036de11b44
Nico Rogalski (msg) <nico.rogalski@msg-david.de>
2022-01-12 18:22:50 +0100

    Init

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,13 @@
+oh-my-posh --init --shell pwsh --config ~/.config/omp/roalsky.omp.json | Invoke-Expression
+
+Import-Module PSReadLine
+Import-Module -Name Terminal-Icons
+
+# Shows navigable menu of all options when hitting Tab
+Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
+
+Set-PSReadLineOption -HistorySearchCursorMovesToEnd
+Set-PSReadLineOption -ShowToolTips
+Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineOption -PredictionViewStyle ListView
+Set-PSReadLineOption -Colors @{ ListPrediction = "#3465a4"}
